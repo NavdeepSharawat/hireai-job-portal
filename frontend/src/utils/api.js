c1d19622ba04dacfb305ff85b7df4082b9b0c973
Navdeep <navdeepsharawat01@email.com>
2026-05-28 17:46:11 +0530

    Update API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: "baseURL: "https://hireai-backend-seuo.onrender.com/api",",
   timeout: 15000,
   headers: { "Content-Type": "application/json" },
 });

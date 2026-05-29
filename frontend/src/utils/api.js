d99b508a6327520db61a5d65d6e6751452ef03d8
Navdeep <navdeepsharawat01@email.com>
2026-05-29 11:06:41 +0530

    Fix API baseURL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "baseURL: "https://hireai-backend-seuo.onrender.com/api",",
+  baseURL: "https://hireai-backend-seuo.onrender.com/api",
   timeout: 15000,
   headers: { "Content-Type": "application/json" },
 });
